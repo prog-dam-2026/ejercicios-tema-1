401979d68516b9246056b8166d164385b6e03d19
alvaroprieto25 <apm2597@gmail.com>
2026-09-17 11:04:46 +0200

    Update template Java file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hola mundo!");
+        ejercicio1();
+        ejercicio2();
+        ejercicio3();
+        ejercicio4();
+        ejercicio5();
+        ejercicio6();
+        ejercicio7();
+        ejercicio8();
+        ejercicio9();
+        ejercicio10();
+    }
+
+    
+    public static void ejercicio1(){
+        // Tu código aquí
+    }
+
+    public static void ejercicio2(){
+
+    }
+
+    public static void ejercicio3(){
+
+    }
+
+    public static void ejercicio4(){
+
+    }
+
+    public static void ejercicio5(){
+
+    }
+
+    public static void ejercicio6(){
+
+    }
+
+    public static void ejercicio7(){
+
+    }
+
+    public static void ejercicio8(){
+
+    }
+
+    public static void ejercicio9(){
+
+    }
+
+    public static void ejercicio10(){
+        
     }
 }
